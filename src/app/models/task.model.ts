5cff4567f39e94f335fca89f43370be0320d3008
Bengi <Bengi>
2026-04-14 16:10:32 -0600

    Crear intefaz de la tarea

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,1 +1,14 @@
-export type TaskStatus = 'pendiente' 
+export type TaskStatus = 'pendiente' | 'en progreso' | 'listo';
+export type TaskPriority = 'baja' | 'media' | 'alta'| 'urgente';
+
+export interface Task {
+    id: string;
+    titulo: string;
+    descripcion: string;
+    fechaCreacion: string;
+    fechaLimite: string;
+    estado: TaskStatus;
+    prioridad: TaskPriority;
+    categoria:string;
+    completada: boolean;
+}
